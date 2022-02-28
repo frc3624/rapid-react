91638ad3440f833b102a025a8e58f703fed7473d
Alice Wang <alicewang@Yingxias-MacBook-Air.local>
2022-02-27 23:45:35 -0500

    Added PathWeaver

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,12 @@
 package frc.robot;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.drive.DriveTrain;
 import frc.robot.commands.RunElevator;
@@ -7,14 +14,19 @@ import frc.robot.commands.climbing.ClimbingDown;
 import frc.robot.commands.climbing.ClimbingUp;
 import frc.robot.commands.shooting.RunShooter;
 import frc.robot.subsystems.Climb;
-import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.PathDrive;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 import static frc.robot.Constants.*;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class RobotContainer {
 	// Controller and Buttons
@@ -26,6 +38,7 @@ public class RobotContainer {
 	
 	// Subsystems
 	private final Drive drive = new Drive();
+	private final PathDrive pathDrive = new PathDrive();
 	private final Climb climb = new Climb();
 	private final Elevator elevator = new Elevator();
 	private final Shooter shooter = new Shooter();
@@ -38,19 +51,59 @@ public class RobotContainer {
 	private final ClimbingUp climbingUp = new ClimbingUp(climb, xboxController);
 	private final RunShooter runShooter = new RunShooter(shooter, elevator, limelight);
 	
+		/**
+	 * Here, we download the path as a json file so it can be traversed by the robot
+	 * The path already specifies voltage/speed/acceleration constraints
+	 * unsure if in RobotContainer or in Robot
+	 */
+	String trajectoryJSON = "paths/testautonomous.wpilib.json";
+	Trajectory trajectory = new Trajectory();
+
+	public void robotInit() {
+   		try {
+      		Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+      		trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+   		} catch (IOException ex) {
+      		DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+   		}
+	}
+	
 	public RobotContainer() {
 		configureButtonBindings();
 		drive.setDefaultCommand(driveTrain);
 	}
 
 	private void configureButtonBindings() {
-		climbUpButton.whileHeld(climbingUp);
 		climbDownButton.whileHeld(climbingDown);
+		climbUpButton.whileHeld(climbingUp);
 		elevatorButton.whileHeld(runElevator);
-		shootingButton.whileHeld(runShooter);
 	}
 
 	public Command getAutonomousCommand() {
-		return null;
+		/**
+		 * The ramsete command allows us to execute the autonomous code
+		 */
+		RamseteCommand ramseteCommand =
+			new RamseteCommand(
+				trajectory,
+				pathDrive::getPose,
+				new RamseteController(Constants.kRamseteB, Constants.kRamseteZeta),
+				new SimpleMotorFeedforward(Constants.ksVolts,
+										Constants.kvVoltSecondsPerMeter,
+										Constants.kaVoltSecondsSquaredPerMeter),
+				Constants.kDriveKinematics,
+				pathDrive::getWheelSpeeds,
+				new PIDController(Constants.kPDriveVel, 0, 0),
+				new PIDController(Constants.kPDriveVel, 0, 0),
+				// RamseteCommand passes volts to the callback
+				pathDrive::tankDriveVolts,
+				pathDrive
+		);
+		
+		// Reset odometry to the starting pose of the trajectory.
+		pathDrive.resetOdometry(trajectory.getInitialPose());
+		
+		// Run path following command, then stop at the end.
+		return ramseteCommand.andThen(() -> pathDrive.tankDriveVolts(0, 0));
 	}
 }
--- src/main/java/frc/robot/subsystems/drive/PathDrive.java
+++ src/main/java/frc/robot/subsystems/drive/PathDrive.java
@@ -1,6 +1,108 @@
 package frc.robot.subsystems.drive;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import frc.robot.Constants;
+
 // Autonomous Driving routine which follows a path
 public class PathDrive extends Drive{
-	public PathDrive() {}
+	public PathDrive() {
+		m_leftEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
+    	m_rightEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
+
+		resetEncoders();
+    	m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d());
+	}
+	//Left drive encoder- measures rotation of wheels
+	private final Encoder m_leftEncoder =
+      new Encoder(
+          Constants.kLeftEncoderPorts[0],
+          Constants.kLeftEncoderPorts[1],
+          Constants.kLeftEncoderReversed);
+
+	//right drive encoder- measures rotation of wheels
+  	private final Encoder m_rightEncoder =
+      new Encoder(
+          Constants.kRightEncoderPorts[0],
+          Constants.kRightEncoderPorts[1],
+          Constants.kRightEncoderReversed);
+    
+	//gyroscope measures the change in the robot's heading
+	private final Gyro m_gyro = new ADXRS450_Gyro();
+
+	//the odometry tracks the robot's pose
+	private final DifferentialDriveOdometry m_odometry;
+
+	@Override
+  	public void periodic() {
+    // Update the odometry in the periodic block for every main loop iteration
+    m_odometry.update(
+        m_gyro.getRotation2d(), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+  	}
+
+	//returns current pose
+	public Pose2d getPose() {
+		return m_odometry.getPoseMeters();
+	}
+
+	//returns current wheel speeds
+	public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+		return new DifferentialDriveWheelSpeeds(m_leftEncoder.getRate(), m_rightEncoder.getRate());
+	}
+
+	//returns current heading
+	public double getHeading() {
+		return m_gyro.getRotation2d().getDegrees();
+	}
+
+	//resets odometry to specified pose
+	public void resetOdometry(Pose2d pose) {
+		resetEncoders();
+		m_odometry.resetPosition(pose, m_gyro.getRotation2d());
+	}
+
+	//controls motors through setting voltages
+	public void tankDriveVolts(double leftVolts, double rightVolts) {
+		leftMaster.setVoltage(leftVolts);
+		rightMaster.setVoltage(rightVolts);
+		diffDrive.feed();
+	}
+
+	//resets drive encoders to read a position of 0
+	public void resetEncoders() {
+		m_leftEncoder.reset();
+		m_rightEncoder.reset();
+	}
+
+	//returns average of 2 encoder readings
+	public double getAverageEncoderDistance() {
+		return (m_leftEncoder.getDistance() + m_rightEncoder.getDistance()) / 2.0;
+	}
+
+	public Encoder getLeftEncoder() {
+		return m_leftEncoder;
+	}
+
+	public Encoder getRightEncoder() {
+		return m_rightEncoder;
+	}
+
+	//sets the max output of the drive, used for scaling speeds
+	public void setMaxOutput(double maxOutput) {
+		diffDrive.setMaxOutput(maxOutput);
+	}
+
+	//resets heading to 0
+	public void zeroHeading() {
+		m_gyro.reset();
+	}
+
+	//returns robot's current turn rate
+	public double getTurnRate() {
+		return -m_gyro.getRate();
+	}
 }
